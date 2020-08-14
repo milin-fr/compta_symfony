c3bb9a718b772d398d4ffbf7f55efe0a0c70aa08
unknown <m.ilin.fr@gmail.com>
2020-08-14 12:45:56 +0200

    MainController draft

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Bill;
+use App\Form\BillType;
+use App\Repository\BillRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function home(BillRepository $billRepository): Response
+    {
+        return $this->render('bill/index.html.twig', [
+            'bills' => $billRepository->findAll(),
+        ]);
+    }
+}
